a7d73b72d66f47a8e30c0a1c8cd614fddcff0f28
enabhandari1988 <48949085+enabhandari1988@users.noreply.github.com>
2019-07-03 08:54:41 +1200

    Update runtests.cmd

--- runtests.cmd
+++ runtests.cmd
@@ -6,7 +6,7 @@
     echo "MSBuild is not in your PATH. Please use a developer command prompt!"
     goto :end
 ) ELSE (
-    MSBuild.exe "SpecFlow.Examples.StepArgumentTransformation.csproj"
+    MSBuild.exe "SphereTest.csproj"
 )
 
 @if ERRORLEVEL 1 goto end
